8fb074c818c1bdf03a3a54424a79f9c368c45857
sobel <nppstcc1001050311@gmail.com>
2025-03-06 01:09:06 +0800

    fix date format bug

--- DiscordBot.cs
+++ DiscordBot.cs
@@ -22,7 +22,7 @@ class DCBot(Configure configure)
         };
 
         _client = new DiscordSocketClient(DC_config);
-        _client.MessageReceived += CommandHandler;
+        _client.MessageReceived += CoMMandHandler;
         _client.Log += Log;
 
         await _client.LoginAsync(TokenType.Bot, configure.ApiKey);
@@ -44,17 +44,17 @@ class DCBot(Configure configure)
     {
         JobManager.AddJob(async () =>
         {
-            string message = $"現在時間：{DateTime.Now.ToString("yyyy/mm/dd")}\n請A班同仁於1555時將卡巴病毒報告匯出成PDF";
+            string message = $"現在時間：{DateTime.Now.ToString("yyyy/MM/dd")}\n請A班同仁於1555時將卡巴病毒報告匯出成PDF";
             await Notify(configure.Channel_sobel_haiyaa_dev__general, message);
         }, s => s.ToRunEvery(1).Days().At(15, 55));
         JobManager.AddJob(async () =>
         {
-            string message = $"現在時間：{DateTime.Now.ToString("yyyy/mm/dd")}\n請B班同仁於2355時將卡巴病毒報告匯出成PDF\n匯出完後將當天全部的PDF檔案寄outlook給管制官";
+            string message = $"現在時間：{DateTime.Now.ToString("yyyy/MM/dd")}\n請B班同仁於2355時將卡巴病毒報告匯出成PDF\n匯出完後將當天全部的PDF檔案寄outlook給管制官";
             await Notify(configure.Channel_sobel_haiyaa_dev__general, message);
         }, s => s.ToRunEvery(1).Days().At(23, 55));
         JobManager.AddJob(async () =>
         {
-            string message = $"現在時間：{DateTime.Now.ToString("yyyy/mm/dd")}\n請C班同仁於0755時將卡巴病毒報告匯出成PDF";
+            string message = $"現在時間：{DateTime.Now.ToString("yyyy/MM/dd")}\n請C班同仁於0755時將卡巴病毒報告匯出成PDF";
             await Notify(configure.Channel_sobel_haiyaa_dev__general, message);
         }, s => s.ToRunEvery(1).Days().At(07, 55));
     }
@@ -63,7 +63,7 @@ class DCBot(Configure configure)
     {
         JobManager.AddJob(async () =>
         {
-            await Notify(configure.Channel_sobel_haiyaa_dev__general, $"中央報時\n現在時間：{DateTime.Now.ToString("yyyy/mm/dd")}");
+            await Notify(configure.Channel_sobel_haiyaa_dev__general, $"中央報時\n現在時間：{DateTime.Now.ToString("yyyy/MM/dd")}");
         }, s => s.ToRunEvery(1).Hours());
     }
 
@@ -100,12 +100,12 @@ class DCBot(Configure configure)
         return Task.CompletedTask;
     }
 
-    // command sample -> 「!6」
-    private Task CommandHandler(SocketMessage message)
+    // coMMand sample -> 「!6」
+    private Task CoMMandHandler(SocketMessage message)
     {
 
-        string command = "";
-        int lengthOfCommand = -1;
+        string coMMand = "";
+        int lengthOfCoMMand = -1;
 
         if (!message.Content.StartsWith('!'))
             return Task.CompletedTask;
@@ -114,14 +114,14 @@ class DCBot(Configure configure)
             return Task.CompletedTask;
 
         if (message.Content.Contains(' '))
-            lengthOfCommand = message.Content.IndexOf(' ');
+            lengthOfCoMMand = message.Content.IndexOf(' ');
         else
-            lengthOfCommand = message.Content.Length;
+            lengthOfCoMMand = message.Content.Length;
 
-        command = message.Content.Substring(1, lengthOfCommand - 1);
+        coMMand = message.Content.Substring(1, lengthOfCoMMand - 1);
 
 
-        switch (command)
+        switch (coMMand)
         {
             case "6":
                 message.Channel.SendMessageAsync($@"屌你老母 {message.Author.Mention}");
