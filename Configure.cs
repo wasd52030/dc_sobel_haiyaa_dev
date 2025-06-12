3a5e9e802c895b9e0a79358cd8747d6722fe7a65
sobel <nppstcc1001050311@gmail.com>
2025-06-12 22:30:17 +0800

    refactoring variable name

--- Configure.cs
+++ Configure.cs
@@ -1,11 +1,11 @@
 internal class Configure
 {
     public string ApiKey { get; set; }
-    public string Channel_sobel_haiyaa_dev__general { get; set; }
+    public string sudo_ntpdate_ChannelID { get; set; }
 
     public override string ToString()
     {
-        return $"ApiKey={ApiKey} Channel_sobel_haiyaa_dev__general={Channel_sobel_haiyaa_dev__general}";
+        return $"ApiKey={ApiKey} sudo_ntpdate_ChannelID={sudo_ntpdate_ChannelID}";
     }
 }
 
